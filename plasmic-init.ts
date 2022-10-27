f95b53c9bbfd5894df17a3bbfd8c27fc605dc78a
pujaexe <puja.exe@gmail.com>
2022-10-27 15:13:12 +0800

    add chakra

---
+++ plasmic-init.ts
@@ -0,0 +1,11 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "9khUWmozN1iqiiTyAwvqXU",  
+      token: "wApW0kP50waiBtOu7R1LbFTZddmNS9jPc1Aj4ozsXqurFgfOg1t8b9L40WucrLp249X2HAwvvnnFcmyjoQ"  
+    }
+  ],
+
+  preview: true,
+})
